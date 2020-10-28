f1e3601e2f1bbac5a9472ec24431f6e0c81b75b4
AhmedAbogameel <gamilco81@gmail.com>
2020-10-29 01:12:22 +0200

    Messages

---
+++ lib/views/messages/chat_tile.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:pet_adoption/views/chat/view.dart';
+
+import '../../constants.dart';
+
+class ChatTile extends StatelessWidget {
+  final String photoUrl;
+  final String name;
+
+  ChatTile({this.photoUrl, this.name});
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return InkWell(
+      splashColor: kAccentColor.withOpacity(0.5),
+      borderRadius: BorderRadius.circular(20),
+      child: Card(
+        color: kBGColor,
+        margin: EdgeInsets.all(10),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15,horizontal: 10),
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 30,
+                backgroundImage: NetworkImage('https://alkhabaralyemeni.net/wp-content/uploads/2020/03/image-2-1.jpg'),
+              ),
+              SizedBox(width: 15,),
+              Flexible(child: Text('Ahmed Abogameel',softWrap: false,overflow: TextOverflow.fade,style: textTheme.title.copyWith(fontSize: 20),))
+            ],),
+        ),
+      ),
+      onTap: ()=> Navigator.push(context, MaterialPageRoute(builder: (_)=> ChatView())),
+    );
+  }
+}
--- lib/views/messages/view.dart
+++ lib/views/messages/view.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:pet_adoption/constants.dart';
+import 'package:pet_adoption/views/messages/chat_tile.dart';
 
 class MessagesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Messages'),
+        // ignore: deprecated_member_use
+        title: Text('Messages',style: textTheme.subtitle,),
         backgroundColor: kBGColor,
         iconTheme: IconThemeData(color: kAccentColor),
       ),
-      body: ListView.builder(
+      body: AnimationLimiter(
+        child: ListView.builder(
+          padding: EdgeInsets.fromLTRB(25, 25, 25, 0),
           itemCount: 3,
-          itemBuilder: (_,listIndex)=>
-        Card()
+          itemBuilder: (_, listIndex) => AnimationConfiguration.staggeredList(
+            position: listIndex,
+            duration: const Duration(seconds: 1),
+            child: SlideAnimation(
+              verticalOffset: 100.0,
+              child: FadeInAnimation(
+                child: ChatTile(),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
